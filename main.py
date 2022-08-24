7bb1876b6bc5bad78dfe24d904943257bad7becd
vedant-204 <katyayana2003@gmail.com>
2022-08-24 23:18:01 +0530

    modified main.py

--- main.py
+++ main.py
@@ -33,4 +33,5 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 
 # main driver function
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
